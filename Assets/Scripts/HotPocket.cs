fb3031c5c88ae141075e737bd02dfc7200ebf255
JVM3 <146955184+JoeMuszynski@users.noreply.github.com>
2025-11-23 22:00:15 -0500

    Hot Pocket script (needs timer update)

---
+++ Assets/Scripts/HotPocket.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotPocket : MonoBehaviour
+{
+    //public GameObject timer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            FindObjectOfType<Timer>().AddTime(5f);
+            FindObjectOfType<Timer>().SetTimerMultiplier(-0.10f);
+                
+            Destroy(gameObject);
+        }
+    }
+}
